72529b26fbb49e4d362333aea6387341902db133
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2026-02-20 13:20:19 +0100

    fix hydration error

--- app/ui/navbar/theme-toggle.tsx
+++ app/ui/navbar/theme-toggle.tsx
@@ -4,48 +4,35 @@ import Image from "next/image";
 import startViewTransitionWrapper from "@/app/lib/start-view-transition-wrapper";
 
 const ThemeToggle = () => {
-  const { setTheme, theme } = useTheme();
-  const isDarkMode = theme === "dark";
+  const { setTheme, resolvedTheme } = useTheme();
 
   const toggleDarkMode = () => {
-    startViewTransitionWrapper(() => setTheme(isDarkMode ? "light" : "dark"));
+    startViewTransitionWrapper(() =>
+      setTheme(resolvedTheme === "dark" ? "light" : "dark"),
+    );
   };
 
-  if (!isDarkMode) {
-    return (
-      <button
-        onClick={toggleDarkMode}
-        aria-label="Switch to dark mode"
-        className="color-white text-black dark:text-white cursor-pointer p-2 ml-5"
-      >
-        <Image
-          src="/sun.svg"
-          width={24}
-          height={24}
-          alt="Switch to dark mode"
-          className="hover:fill-yellow-400"
-        />
-      </button>
-    );
-  }
-
-  if (isDarkMode) {
-    return (
-      <button
-        onClick={toggleDarkMode}
-        aria-label="Switch to light mode"
-        className="color-white text-black dark:text-white cursor-pointer p-2 ml-5"
-      >
-        <Image
-          src="/moon.svg"
-          width={24}
-          height={24}
-          alt="Switch to light mode"
-          className="dark:invert"
-        />
-      </button>
-    );
-  }
+  return (
+    <button
+      onClick={toggleDarkMode}
+      className="color-white text-black dark:text-white cursor-pointer p-2 ml-5"
+    >
+      <Image
+        src="/sun.svg"
+        width={24}
+        height={24}
+        alt="Switch to dark mode"
+        className="dark:hidden hover:fill-yellow-400"
+      />
+      <Image
+        src="/moon.svg"
+        width={24}
+        height={24}
+        alt="Switch to light mode"
+        className="hidden dark:block dark:invert"
+      />
+    </button>
+  );
 };
 
 export default ThemeToggle;
