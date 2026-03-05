0dc18c2667412b42ac447af6623449a593480006
Marcus Mohns <92151780+MarcusMohns@users.noreply.github.com>
2026-03-05 10:38:23 +0100

    font cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
 import Navbar from "./ui/layout/navbar";
 import Footer from "./ui/layout/footer";
 
-const geistSans = Geist({
+import { Geist } from "next/font/google";
+const geist = Geist({
+  subsets: ["latin"],
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -30,15 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning className={geist.className}>
+      <body className="antialiased bg-stone-50 text-stone-900 dark:bg-stone-900 dark:text-stone-50">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <main
             className="flex flex-col justify-center items-center xl:p-10
-    min-h-screen w-full md:pt-15"
+    min-h-screen w-full md:pt-16"
           >
             {children}
           </main>
